6b92dd78f6feb223f89cca0e29d61c2e5e294397
Abdiel Brilhante <abdielbrilhante@gmail.com>
2018-11-06 21:22:12 -0300

    v0.1.0

---
+++ src/lib/config.js
@@ -0,0 +1,38 @@
+import { on, publish } from './events'
+import * as transform from './transform'
+
+let availableLocales = {}
+
+let data = {
+	locale: '',
+	currency: '',
+	dictionary: {},
+}
+
+export function config(locales) {
+	availableLocales = { ...locales }
+	publish('onConfig')
+}
+
+export function set(iso) {
+	const localeData = availableLocales[iso]
+
+	if (localeData) {
+		data = { ...localeData }
+		publish('onLocaleChange', data)
+	}
+
+	return data
+}
+
+export function full() {
+	return data
+}
+
+const withConfig = fn => (...args) => fn(full(), ...args)
+
+export const t = withConfig(transform.t)
+export const c = withConfig(transform.c)
+export const n = withConfig(transform.n)
+
+export const onLocaleChange = fn => on('onLocaleChange', fn)
---
+++ src/lib/transform.js
@@ -0,0 +1,44 @@
+function getPhrase (config, path = '', count) {
+  const keys = path.split('.')
+
+  let node = config.dictionary
+  try {
+    for (let i = 0; i < keys.length; i++) {
+      node = node[keys[i]]
+    }
+
+    if (Array.isArray(node)) {
+      return node[Math.min(count || 0, 2)]
+    } else {
+      return node
+    }
+  } catch (e) {
+    console.error(`[i18n] ${path} not found`)
+    return path
+  }
+}
+
+export function t (config, path, params = {}) {
+  let phrase = getPhrase(config, path, params.count)
+
+  for (const key of Object.keys(params)) {
+    const regex = new RegExp(`{${key}}`, 'g')
+    phrase = phrase.replace(regex, params[key])
+  }
+
+  return phrase
+}
+
+export function c (config, value = 0) {
+  const { locale, currency } = config
+  return value.toLocaleString(locale, {
+    style: 'currency',
+    currency,
+  })
+}
+
+export function n (config, value = '') {
+  const digits = value.replace(/[^0-9]+/g, '')
+  const cutoff = digits.length - 2
+  return parseFloat(`${digits.slice(0, cutoff)}.${digits.slice(cutoff)}`)
+}
